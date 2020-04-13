67ba669a88f11cdf5e274e840c30574df516894e
Averkov-Alexander <62053463+Averkov-Alexander@users.noreply.github.com>
2020-04-13 23:50:11 +0300

    Update otus_hw_07.sql

--- otus_hw_07.sql
+++ otus_hw_07.sql
@@ -1,5 +1,5 @@
 USE WideWorldImporters;
---1. Довставлять в базу 5 записей используя insert в таблицу Customers или Suppliers
+--1. Г„Г®ГўГ±ГІГ ГўГ«ГїГІГј Гў ГЎГ Г§Гі 5 Г§Г ГЇГЁГ±ГҐГ© ГЁГ±ГЇГ®Г«ГјГ§ГіГї insert Гў ГІГ ГЎГ«ГЁГ¶Гі Customers ГЁГ«ГЁ Suppliers
 IF NOT EXISTS(SELECT TOP 1 1 FROM Sales.Customers WHERE CustomerName LIKE '%W07')
 BEGIN
 INSERT INTO Sales.Customers
@@ -32,17 +32,17 @@ VALUES
 	('Toyota #HW07', 1, 3,3134,3,24805,24805,2100.00,'2013-01-01',0.000,0,0,7,'(218) 555-0100','(218) 555-0100','www.website.com','Shop 7','548 Bures Crescent','90647','Shop 7','548 Bures Crescent','90647',1),
 	('Volkswagen #HW07', 1, 3,3134,3,24805,24805,2100.00,'2013-01-01',0.000,0,0,7,'(218) 555-0100','(218) 555-0100','www.website.com','Shop 7','548 Bures Crescent','90647','Shop 7','548 Bures Crescent','90647',1);
 END;
---проверим, что записи добавились
+--ГЇГ°Г®ГўГҐГ°ГЁГ¬, Г·ГІГ® Г§Г ГЇГЁГ±ГЁ Г¤Г®ГЎГ ГўГЁГ«ГЁГ±Гј
 SELECT * FROM Sales.Customers WHERE CustomerName Like '%HW07%';
---2. удалите 1 запись из Customers, которая была вами добавлена
+--2. ГіГ¤Г Г«ГЁГІГҐ 1 Г§Г ГЇГЁГ±Гј ГЁГ§ Customers, ГЄГ®ГІГ®Г°Г Гї ГЎГ»Г«Г  ГўГ Г¬ГЁ Г¤Г®ГЎГ ГўГ«ГҐГ­Г 
 DELETE FROM Sales.Customers
 WHERE CustomerName = 'Volkswagen #HW07'; 
---3. изменить одну запись, из добавленных через UPDATE
+--3. ГЁГ§Г¬ГҐГ­ГЁГІГј Г®Г¤Г­Гі Г§Г ГЇГЁГ±Гј, ГЁГ§ Г¤Г®ГЎГ ГўГ«ГҐГ­Г­Г»Гµ Г·ГҐГ°ГҐГ§ UPDATE
 UPDATE Sales.Customers
 Set CustomerName = CustomerName + ' #UPD'
 FROM Sales.Customers
 WHERE CustomerName = 'Toyota #HW07'
---4. Написать MERGE, который вставит запись в клиенты, если ее там нет, и изменит если она уже есть
+--4. ГЌГ ГЇГЁГ±Г ГІГј MERGE, ГЄГ®ГІГ®Г°Г»Г© ГўГ±ГІГ ГўГЁГІ Г§Г ГЇГЁГ±Гј Гў ГЄГ«ГЁГҐГ­ГІГ», ГҐГ±Г«ГЁ ГҐГҐ ГІГ Г¬ Г­ГҐГІ, ГЁ ГЁГ§Г¬ГҐГ­ГЁГІ ГҐГ±Г«ГЁ Г®Г­Г  ГіГ¦ГҐ ГҐГ±ГІГј
 MERGE Sales.Customers AS Target
 USING (
 SELECT
@@ -86,11 +86,11 @@ WHEN NOT MATCHED
 	THEN INSERT (CustomerID, CustomerName, BillToCustomerID, CustomerCategoryID, PrimaryContactPersonID, DeliveryMethodID, DeliveryCityID, PostalCityID, CreditLimit, AccountOpenedDate, StandardDiscountPercentage, IsStatementSent, IsOnCreditHold, PaymentDays, PhoneNumber, FaxNumber, WebsiteURL, DeliveryAddressLine1, DeliveryAddressLine2, DeliveryPostalCode, PostalAddressLine1, PostalAddressLine2, PostalPostalCode, LastEditedBy)
 		VALUES (source.CustomerID, source.CustomerName, source.BillToCustomerID, source.CustomerCategoryID, source.PrimaryContactPersonID, source.DeliveryMethodID, source.DeliveryCityID, source.PostalCityID, source.CreditLimit, source.AccountOpenedDate, source.StandardDiscountPercentage, source.IsStatementSent, source.IsOnCreditHold, source.PaymentDays, source.PhoneNumber, source.FaxNumber, source.WebsiteURL, source.DeliveryAddressLine1, source.DeliveryAddressLine2, source.DeliveryPostalCode, source.PostalAddressLine1, source.PostalAddressLine2, source.PostalPostalCode, source.LastEditedBy)
 OUTPUT $action, deleted.*, inserted.*;
---5. Напишите запрос, который выгрузит данные через bcp out и загрузить через bulk insert
--- для корректной работы примера необходимо ниже по тексту заменить:
---а) имя файла в который будут выгружены данные на своё:
+--5. ГЌГ ГЇГЁГёГЁГІГҐ Г§Г ГЇГ°Г®Г±, ГЄГ®ГІГ®Г°Г»Г© ГўГ»ГЈГ°ГіГ§ГЁГІ Г¤Г Г­Г­Г»ГҐ Г·ГҐГ°ГҐГ§ bcp out ГЁ Г§Г ГЈГ°ГіГ§ГЁГІГј Г·ГҐГ°ГҐГ§ bulk insert
+-- Г¤Г«Гї ГЄГ®Г°Г°ГҐГЄГІГ­Г®Г© Г°Г ГЎГ®ГІГ» ГЇГ°ГЁГ¬ГҐГ°Г  Г­ГҐГ®ГЎГµГ®Г¤ГЁГ¬Г® Г­ГЁГ¦ГҐ ГЇГ® ГІГҐГЄГ±ГІГі Г§Г Г¬ГҐГ­ГЁГІГј:
+--Г ) ГЁГ¬Гї ГґГ Г©Г«Г  Гў ГЄГ®ГІГ®Г°Г»Г© ГЎГіГ¤ГіГІ ГўГ»ГЈГ°ГіГ¦ГҐГ­Г» Г¤Г Г­Г­Г»ГҐ Г­Г  Г±ГўГ®Вё:
 --C:\Program Files\Microsoft SQL Server\MSSQL13.SQLEXPRESS\MSSQL\Backup\Customers.txt
---б) имя сервера на своё
+--ГЎ) ГЁГ¬Гї Г±ГҐГ°ГўГҐГ°Г  Г­Г  Г±ГўГ®Вё
 --HP-17AK014UR\SQLEXPRESS
 
 EXEC sp_configure 'show advanced options', 1;  
@@ -103,7 +103,7 @@ RECONFIGURE;
 GO
 exec master..xp_cmdshell 'BCP "SELECT TOP 10 * FROM WideWorldImporters.Sales.Customers" queryout "C:\Program Files\Microsoft SQL Server\MSSQL13.SQLEXPRESS\MSSQL\Backup\Customers.txt" -T -c -S HP-17AK014UR\SQLEXPRESS';
 
-SELECT * INTO Customers_Copy FROM Sales.Customers WHERE 0=1; --создаём копию таблицы
+SELECT * INTO Customers_Copy FROM Sales.Customers WHERE 0=1; --Г±Г®Г§Г¤Г ВёГ¬ ГЄГ®ГЇГЁГѕ ГІГ ГЎГ«ГЁГ¶Г»
 --SELECT * FROM Customers_Copy;
 BULK INSERT Customers_Copy
    FROM "C:\Program Files\Microsoft SQL Server\MSSQL13.SQLEXPRESS\MSSQL\Backup\Customers.txt"
@@ -114,7 +114,7 @@ BULK INSERT Customers_Copy
 		KEEPNULLS,
 		TABLOCK        
 	  );
---проверим, что данные в таблицу загрузились
+--ГЇГ°Г®ГўГҐГ°ГЁГ¬, Г·ГІГ® Г¤Г Г­Г­Г»ГҐ Гў ГІГ ГЎГ«ГЁГ¶Гі Г§Г ГЈГ°ГіГ§ГЁГ«ГЁГ±Гј 
 SELECT * FROM Customers_Copy;
---удалим таблицу
+--ГіГ¤Г Г«ГЁГ¬ ГІГ ГЎГ«ГЁГ¶Гі
 DROP TABLE Customers_Copy;
